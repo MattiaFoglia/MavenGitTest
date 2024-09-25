daf6b634f15883deb4fc835c1270d9f4e683d57c
lai2-24 <lai2-24@itismeucci.com>
2024-09-25 12:20:33 +0200

    second commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,18 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+        int l =100;
+        MyThread t1 = new MyThread(l);
+        MyThread t2 = new MyThread(l);
+        new Thread(t1).start();
+        new Thread(t2).start();
+    //  t1.start();
+    //  t2.start();
         System.out.println("Hello world!");
+
+     //   for(int i=0; i<100 ;i++){
+     //       System.out.println(Thread.currentThread().getName() + ": " + i);
+     //   }
     }
 }
+    
